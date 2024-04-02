8e2b172d764917a8caa7cd335662e6e9ed69ae82
Elvio Jardini <elvio.jardini@mazzafc.tech>
2024-04-01 22:44:19 -0300

    feat: arquivos de apoio

---
+++ helpers/registerPage/registerPageMessages.ts
@@ -0,0 +1,9 @@
+export const registerPageMessages = {
+    firstNameRequired: "First name is required.",
+    lastNameRequired: "Last name is required.",
+    emailRequired: "Email is required.",
+    wrongEmail: "Wrong email",
+    passwordRequired: "Password is required.",
+    passwordRules: "Password must meet the following rules: must have at least 6 characters",
+    passwordNotMatch: "The password and confirmation password do not match."
+}
